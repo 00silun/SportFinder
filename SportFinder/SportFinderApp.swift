80808ffa65440b226f494dfafa6e6af0e661e439
Simon Lundgren <simon00.lundgren@gmail.com>
2024-02-24 14:34:44 +0100

    Initial Commit

---
+++ SportFinder/SportFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportFinderApp.swift
+//  SportFinder
+//
+//  Created by Simon Lundgren on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SportFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
